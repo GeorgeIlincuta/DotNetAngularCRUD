24114ea768dbf5192ecca1d1d06058e9aab62cd9
Cosmin Ilincuta <ilincuta.cosmin@gmail.com>
2022-02-04 13:37:01 +0200

    removing unecesary code

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,17 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace API
 {
